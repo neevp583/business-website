fe37beff40aca2fb18603f6e6786c68c0f6de377
neevp583 <neevp583@gmail.com>
2026-02-11 11:46:21 +0530

    emailjs fix

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-function msg() {
-  alert("Thanks for contacting us 😄");
-}
+// EmailJS init
+(function(){
+   emailjs.init("Kw9rNFgZ0913SuCne");
+})();
+
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    emailjs.sendForm(
+        "service_website",     // Service ID
+        "template_y4lq4z7",     // Template ID
+        this
+    ).then(function() {
+        alert("Message sent successfully! ✅");
+    }, function(error) {
+        alert("Failed... ❌ " + error.text);
+    });
+});
+  
