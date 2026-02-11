436a572feac3c91de3e1da5850dec59901ff8187
neevp583 <neevp583@gmail.com>
2026-02-11 12:03:07 +0530

    fix template id

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-// EmailJS init
-(function(){
-   emailjs.init("Kw9rNFgZ0913SuCne");
-})();
-
-document.getElementById("contact-form").addEventListener("submit", function(e) {
-    e.preventDefault();
-
-    emailjs.sendForm(
-        "service_website",     // Service ID
-        "template_y4lq4z7",     // Template ID
-        this
-    ).then(function() {
-        alert("Message sent successfully! ✅");
-    }, function(error) {
-        alert("Failed... ❌ " + error.text);
-    });
-});
-  
+emailjs.sendForm(
+  "service_website",
+  "y4lq4z7",   // ✅ sirf ID
+  this
+)
