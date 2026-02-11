00377f9d19afa32420e2e4ee53993ec1da6b4707
neevp583 <neevp583@gmail.com>
2026-02-11 12:12:32 +0530

    fix reload issue

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+(function(){
+  emailjs.init("Kw9rNFgZ0913SuCne"); // Public key
+})();
+
+document.getElementById("contact-form").addEventListener("submit", function(e){
+  e.preventDefault(); // Page reload stop
+
+  emailjs.sendForm(
+    "service_website",
+    "template_y4lq4z7",
+    this
+  ).then(
+    function(){
+      alert("Message sent successfully ✅");
+    },
+    function(error){
+      alert("Failed ❌ " + error.text);
+    }
+  );
+});
 emailjs.sendForm(
   "service_website",
   "y4lq4z7",   // ✅ sirf ID
